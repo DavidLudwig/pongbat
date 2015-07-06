792f5771cf61ccbd358ad694d8b2c2f4dc8985f6
David Ludwig <dludwig@pobox.com>
2015-07-06 09:49:43 -0400

    Emscripten build-script improvements: install via scp; ignore install process if compile fails; easier install-dir setting

--- build-scripts/emscripten.sh
+++ build-scripts/emscripten.sh
@@ -4,6 +4,23 @@
 PROJECT_DIR=$( cd -P "$( dirname "$0" )/.." && pwd )
 BUILD_DIR="$PROJECT_DIR/build/emscripten"
 
+echo "BUILD_DIR: $BUILD_DIR"
+
+# Setup + log install-dir details
+if [ "$1" != "" ]; then
+	PONGBAT_INSTALL_DIR="$1"
+fi
+if [ "$PONGBAT_INSTALL_DIR" != "" ]; then
+	echo "Install path: $PONGBAT_INSTALL_DIR"
+fi
+
+if ! type -p scp > /dev/null; then
+	echo "Warning: Can't find 'scp'.  Installation to remote directories will be disabled"
+	CP=cp
+else
+	CP=scp
+fi
+
 # Setup shell for Emscripten SDK use:
 if ! type -p emsdk_env.sh > /dev/null ; then
 	echo "Can't find emsdk_env.sh (part of Emscripten SDK)!"
@@ -25,14 +42,22 @@ rm -rf *
 
 # Rebuild
 echo "Building..."
-emcc ../../src/main.cpp -s USE_SDL=2 -O3 -o pongbat.html
+if ! emcc ../../src/main.cpp -s USE_SDL=2 -O3 -o pongbat.html; then
+	echo "Script exiting, as compile failed"
+	exit 1
+fi
 
 # Install
-echo "Installing..."
-if [ -d "$PONGBAT_INSTALL_DIR" ]; then
-	cp -rv * "$PONGBAT_INSTALL_DIR/"
+if [ "$PONGBAT_INSTALL_DIR" != "" ]; then
+	echo "Installing..."
+	if echo "$PONGBAT_INSTALL_DIR" | grep -q ":" > /dev/null; then
+		"$CP" -r * "$PONGBAT_INSTALL_DIR/"
+	else
+		# Use verbose logging when a local directory is specified
+		"$CP" -rv * "$PONGBAT_INSTALL_DIR/"
+	fi
 else
-	echo "... Nowhere to install to!  Set 'PONGBAT_INSTALL_DIR' in env to an already-mkdir'ed install location."
+	echo "Ignoring install, as neither the env-var PONGBAT_INSTALL_DIR, nor the build-script's 1st arg, were set."
 fi
 
 echo "Script is complete ( $0 )"
